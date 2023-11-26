0c524fb70d2552ff2e969052939cb4afdcf9aef4
tonigith <91883442+toniGitH@users.noreply.github.com>
2023-11-26 09:29:53 +0100

    Added: view message (success & error) to be printed in createTask.phtml script

--- app/controllers/ApplicationController.php
+++ app/controllers/ApplicationController.php
@@ -10,55 +10,56 @@
 
 class ApplicationController extends Controller 
 {
+
 	public function getViewInsertFormAction()
         {   
         }
 
-        public function createTaskAction()
-        {
+    public function createTaskAction()
+    {
 
-            if (isset($_GET) && !empty($_GET)) {
+        if (isset($_GET) && !empty($_GET)) {
             
-                // Array where store data collected from the new task creation form
-                $newTaskData = array(
+            // Array where store data collected from the new task creation form
+            $newTaskData = array(
                     "task_name" => $_GET["task_name"],
                     "task_details" => $_GET["task_details"],
                     "task_created_by" => $_GET["task_created_by"],
                     "task_deadline" => $_GET["task_deadline"],
                     "task_assigned_to" => $_GET["task_assigned_to"],
-                );
-                
-                // Read the current content of the JSON file
-                $jsonContent = file_get_contents('../db/dataBase.json');
+            );
+            
+            // Read the current content of the JSON file
+            $jsonContent = file_get_contents('../db/dataBase.json');
 
-                // Decode the JSON file into a Task objects array   
-                $tasks = json_decode($jsonContent, true, 512, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+            // Decode the JSON file into a Task objects array   
+            $tasks = json_decode($jsonContent, true, 512, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
 
-                // Create a new Task object with the data to insert
-                $newTask = new TaskModel(
+            // Create a new Task object with the data to insert
+            $newTask = new TaskModel(
                     $newTaskData["task_name"],
                     $newTaskData["task_details"],
                     $newTaskData["task_created_by"],
                     $newTaskData["task_deadline"],
                     $newTaskData["task_assigned_to"],
-                );
+            );
 
-                // Add new Task object into the array
-                $tasks[] = $newTask;
+            // Add new Task object into the array
+            $tasks[] = $newTask;
 
-                // Encode Task objects array back to JSON
-                $newJsonContent = json_encode($tasks, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+            // Encode Task objects array back to JSON
+            $newJsonContent = json_encode($tasks, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 
-                // Write the new content intoto the JSON file
-                file_put_contents('../db/dataBase.json', $newJsonContent);
+            // Write the new content intoto the JSON file
+            file_put_contents('../db/dataBase.json', $newJsonContent);
 
-                //echo "Success message";
+            // Success message;
+            $this->view->message = "New task created successfully";
 
-            } else {
-                echo "Error: new task could not be created";
-            }
-
-
-
+        } else {
+            // Error message;
+            $this->view->message = "Error: new task could not be created";
         }
+
+    }
 }
