3b7eb0abc73ebdb8e2a095f1819a99cfddef3a0d
tonigith <91883442+toniGitH@users.noreply.github.com>
2023-11-27 11:23:20 +0100

    Added input Start date in getViewInsertForm.php & modified createTastAction in ApplicationController.php in order to validate startdate not greather than deadline

--- app/controllers/ApplicationController.php
+++ app/controllers/ApplicationController.php
@@ -18,24 +18,30 @@ class ApplicationController extends Controller
     public function createTaskAction()
     {
         if (isset($_POST) && !empty($_POST)) {
-                
-            // Array where store data collected from the new task creation form
-            $newTaskData = array(
-                        "task_id"=> uniqid(),
-                        "task_name" => $_POST["task_name"],
-                        "task_details" => $_POST["task_details"],
-                        "task_created_by" => $_POST["task_created_by"],
-                        "task_creation_date" => date('Y-m-d'),
-                        "task_deadline" => $_POST["task_deadline"],
-                        "task_assigned_to" => $_POST["task_assigned_to"],
-                        "task_status" => "Pending"
-            );
-                
+            
+            if ($_POST["task_deadline"] < $_POST["task_startDate"]) {
+                $this->view->message = "Deadline cannot be greater than the Start date!!";
+                $this->view->txtColor = "text-red-500";
+            }
+            else{
+                // Array where store data collected from the new task creation form
+                $newTaskData = array(
+                "task_id"=> uniqid(),
+                "task_name" => $_POST["task_name"],
+                "task_details" => $_POST["task_details"],
+                "task_created_by" => $_POST["task_created_by"],
+                "task_creation_date" => $_POST["task_startDate"],
+                "task_deadline" => $_POST["task_deadline"],
+                "task_assigned_to" => $_POST["task_assigned_to"],
+                "task_status" => "Pending"
+                );
+            
             $taskModel = new TaskModel();
             $taskModel->createTask($newTaskData);
             // Success message;
             $this->view->message = "New task created successfully!!";
             $this->view->txtColor = "text-green-500";
+            }
         }
         else{
             // Error message;
