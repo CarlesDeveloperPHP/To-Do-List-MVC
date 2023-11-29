06692d6a2f6e7cca8eaf6830bad6c5a7526e310d
tonigith <91883442+toniGitH@users.noreply.github.com>
2023-11-29 20:36:55 +0100

    Delete task completed

--- app/models/TaskModel.php
+++ app/models/TaskModel.php
@@ -19,18 +19,16 @@ class TaskModel
     public function getAllTasks()
     {
         // $data= file_get_contents('/..app/models/persistencia/DDBB.json') ;
-        $data = file_get_contents($this->jsonPath);
+        $jsonContent = file_get_contents($this->jsonPath);
         // The second argument 'true' indicates that an associative array should be returned instead of an object.
-        $tasks = json_decode($data, true);
+        $tasks = json_decode($jsonContent, true, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
         return $tasks;
     }
     
     public function createTask($newTaskData)
-    {            
-        // Read the current content of the JSON file
-        $jsonContent = file_get_contents($this->jsonPath);
-        // Decode the JSON file into a Task objects array   
-        $tasks = json_decode($jsonContent, true, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+    {   
+        // Convert data json file into array     
+        $tasks = $this->getAllTasks();
         // Add new Task object into the array
         $tasks[] = $newTaskData;
         // Encode Task objects array back to JSON
@@ -39,13 +37,10 @@ class TaskModel
         file_put_contents('../db/dataBase.json', $newJsonContent);
     }
 
-    public function getTaskById($task_id)
+    public function getTaskIndexById($task_id)
     {
-        // Read the current content of the JSON file
-        $jsonContent = file_get_contents($this->jsonPath);
-
-        // Decode the JSON file into an array of Task objects
-        $tasks = json_decode($jsonContent, true, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+        // Convert data json file into array     
+        $tasks = $this->getAllTasks();
 
         // Find the index of the task to extract
         $taskIndex = -1;
@@ -56,7 +51,16 @@ class TaskModel
             }
             $i++;
         } while ($i < count($tasks) && $taskIndex === -1);
+        
+        return $taskIndex;
+    }
 
+    public function getTaskById($task_id)
+    {
+        // Convert data json file into array     
+        $tasks = $this->getAllTasks();
+        // Get the index of the data corresponding to the task_id
+        $taskIndex = $this->getTaskIndexById($task_id);
         // If the task to extract is found, return the array
         if ($taskIndex !== -1) {
             return $tasks[$taskIndex];
@@ -66,38 +70,25 @@ class TaskModel
     }
     
     public function deleteTask($task_id)
-    {
-        // Read the current content of the JSON file
-        $jsonContent = file_get_contents($this->jsonPath);
-    
-        // Decode the JSON file into an array of Task objects
-        $tasks = json_decode($jsonContent, true, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
-    
-        // Find the index of the task to extract
-        $taskIndex = -1;
-        $i = 0;
-        while ($i < count($tasks) && $taskIndex === -1) {
-            if ($tasks[$i]['task_id'] == $task_id) {
-                $taskIndex = $i;
-            }
-            $i++;
-        }
-    
-        // If the task to delete is found, remove it from the array
+    {        
+        // Convert data json file into array     
+        $tasks = $this->getAllTasks();
+        // Get the index of the data corresponding to the task_id
+        $taskIndex = $this->getTaskIndexById($task_id);
+        // Deleting process
         if ($taskIndex !== -1) {
-            // Remove task from the array tasks
             array_splice($tasks, $taskIndex, 1);
-    
             // Encode the array of Task objects back to JSON
             $newJsonContent = json_encode($tasks, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
-    
             // Write the new content to the JSON file
             file_put_contents('../db/dataBase.json', $newJsonContent);
             return true;
         } else {
-            return "Task not found";
+            return false;
         }
-    }        
+
+    }
+
 }
 
 ?>
